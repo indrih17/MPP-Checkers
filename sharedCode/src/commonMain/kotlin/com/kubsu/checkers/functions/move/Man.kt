da6a124c2eedf6f6ab90e7953edc61e9bdfede96
indrih17 <indrih17@gmail.com>
2019-12-11 02:14:34 +0300

    Bug fix.

--- sharedCode/src/commonMain/kotlin/com/kubsu/checkers/functions/move/Man.kt
+++ sharedCode/src/commonMain/kotlin/com/kubsu/checkers/functions/move/Man.kt
@@ -32,14 +32,14 @@ private fun isSimpleMove(current: Cell.Piece.Man, destination: Cell.Empty): Bool
 
 private infix fun Cell.Piece.Man.toLeftOf(destination: Cell.Empty): Boolean =
     when (color) {
-        Color.White -> row - 1 == destination.row && column - 1 == destination.column
-        Color.Black -> row + 1 == destination.row && column + 1 == destination.column
+        Color.White -> row - 1 == destination.row && column + 1 == destination.column
+        Color.Black -> row + 1 == destination.row && column - 1 == destination.column
     }
 
 private infix fun Cell.Piece.Man.toRightOf(destination: Cell.Empty): Boolean =
     when (color) {
-        Color.White -> row - 1 == destination.row && column + 1 == destination.column
-        Color.Black -> row + 1 == destination.row && column - 1 == destination.column
+        Color.White -> row - 1 == destination.row && column - 1 == destination.column
+        Color.Black -> row + 1 == destination.row && column + 1 == destination.column
     }
 
 private fun isAttack(
